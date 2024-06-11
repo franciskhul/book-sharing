d22170fb0accc6d02eb4f56579ff15cee2068fd4
Francis Njuguna <francisnvinner@gmail.com>
2024-06-11 08:58:04 +0300

    feat: create the Page component.

---
+++ frontend/src/components/Page.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, ReactNode, Ref } from "react"
+import { Box } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+
+interface PageProps {
+    title: string;
+    children?: ReactNode;
+    meta?: ReactNode;
+}
+
+const Page = forwardRef<HTMLDivElement, PageProps>(
+    ({ children, title = '', meta, ...other }, ref: Ref<HTMLDivElement>) => (
+        <>
+            <Helmet>
+                <title>{`${title} | Senga`}</title>
+                {meta}
+            </Helmet>
+
+            <Box ref={ref} {...other}>
+                {children}
+            </Box>
+        </>
+    ));
+
+export default Page;

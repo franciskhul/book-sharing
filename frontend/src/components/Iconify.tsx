f255ea85110c04fc6ebd673aeb7b8c27c8ca7759
Francis Njuguna <francisnvinner@gmail.com>
2024-06-12 10:37:27 +0300

    feat: set up Iconify component.

---
+++ frontend/src/components/Iconify.tsx
@@ -0,0 +1,13 @@
+// icons
+import { Icon } from '@iconify/react';
+// @mui
+import { Box } from '@mui/material';
+
+interface IconifyProps {
+    icon: string,
+    sx: object
+}
+
+export default function Iconify({ icon, sx, ...other }: IconifyProps) {
+    return <Box component={Icon} icon={icon} sx={{ ...sx }} {...other} />;
+}

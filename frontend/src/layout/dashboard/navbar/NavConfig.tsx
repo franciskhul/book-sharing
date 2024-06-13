f0ed3c8ba60b014f449c8e0164463c99f035f613
Francis Njuguna <francisnvinner@gmail.com>
2024-06-13 15:50:44 +0300

    style: add the assigned books and unassigned books in the navbar.

--- frontend/src/layout/dashboard/navbar/NavConfig.tsx
+++ frontend/src/layout/dashboard/navbar/NavConfig.tsx
@@ -1,5 +1,14 @@
+// components
+import { ReactNode } from "react";
+import SvgIconStyle from "../../../components/SvgIconStyle";
 
 const ROOTS_DASHBOARD: string = '/';
+
+const getIcon = (name: string): ReactNode => {
+    // /assets/icons/navbar/ic_unassigned_books.svg
+    console.log(`/assets/icons/navbar/${name}.svg`);
+    return <SvgIconStyle src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+}
 
 function path(root: string, sublink: string) {
     return `${root}${sublink}`;
@@ -15,9 +24,14 @@ const navConfig = [
         subheader: 'Teacher',
         items: [
             {
-                title: 'Books',
+                title: 'Unassigned Books',
                 path: PATH_DASHBOARD.books,
-                icon: <></>
+                icon: getIcon('ic_unassigned_books')
+            },
+            {
+                title: 'Assigned Books',
+                path: PATH_DASHBOARD.books,
+                icon: getIcon('ic_assigned_books')
             }
         ]
     }

4760dbdef466d20eec8aaccff9986740cef702e2
Francis Njuguna <francisnvinner@gmail.com>
2024-06-14 13:18:04 +0300

    bug: add the HelmetProvider.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
+import { HelmetProvider } from 'react-helmet-async';
 import client from './utils/apolloClient';
 import { CollapseDrawerProvider } from './context/CollapseDrawerContext';
 import ThemeProvider from './theme';
@@ -8,14 +9,16 @@ import Router from './routes';
 
 export default function App() {
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider>
-        <CollapseDrawerProvider>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </CollapseDrawerProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <HelmetProvider>
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+          <CollapseDrawerProvider>
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </CollapseDrawerProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </HelmetProvider>
   )
 }

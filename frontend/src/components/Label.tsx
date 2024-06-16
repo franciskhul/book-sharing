c81b49794f47504e0b26b6709be4484a82564505
Francis Njuguna <francisnvinner@gmail.com>
2024-06-16 14:28:15 +0300

    style(Label): improved on the Label component style

--- frontend/src/components/Label.tsx
+++ frontend/src/components/Label.tsx
@@ -1,17 +1,31 @@
 import React, { ReactNode } from "react";
 // @mui
-import { Theme, styled } from '@mui/material/styles';
+import { Theme, styled, alpha } from '@mui/material/styles';
 
 interface LabelProps {
     children: ReactNode;
-    sx: object
+    color: string;
+    sx?: object
 }
 
 interface SpanProps {
     theme?: Theme
+    ownerState: { color: string; }
 }
 
-const RootStyle = styled('span')<SpanProps>(({ theme }) => {
+const RootStyle = styled('span')<SpanProps>(({ theme, ownerState }) => {
+    const { color } = ownerState;
+
+
+    const styleGhost = (color: string) => ({
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        color: theme.palette[color],
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        backgroundColor: alpha(theme.palette[color], 0.16),
+    });
+
     return {
         height: 22,
         minWidth: 22,
@@ -22,19 +36,18 @@ const RootStyle = styled('span')<SpanProps>(({ theme }) => {
         whiteSpace: 'nowrap',
         display: 'inline-flex',
         justifyContent: 'center',
-        padding: theme.spacing(0, 1),
+        padding: theme.spacing(0, 2),
+        fontSize: theme.typography.pxToRem(12),
         fontFamily: theme.typography.fontFamily,
         fontWeight: theme.typography.fontWeightBold,
-        fontSize: theme.typography.pxToRem(12),
-        color: '#212B36',
-        backgroundColor: '##DFE3E8'
+        ...styleGhost(color)
     }
 });
 
 
-const Label: React.FC<LabelProps> = ({ children, sx }) => {
+const Label: React.FC<LabelProps> = ({ children, color, sx }) => {
     return (
-        <RootStyle sx={{ ...sx }}>
+        <RootStyle sx={{ ...sx }} ownerState={{ color }}>
             {children}
         </RootStyle>
     )

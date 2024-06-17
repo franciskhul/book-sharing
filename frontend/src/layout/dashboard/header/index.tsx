9f90c6168c980bd9f61c9754a1f8d1009cdc72c3
Francis Njuguna <francisnvinner@gmail.com>
2024-06-17 11:46:28 +0300

    chore: strip off unnecessary code inside the header/index component.

--- frontend/src/layout/dashboard/header/index.tsx
+++ frontend/src/layout/dashboard/header/index.tsx
@@ -26,22 +26,18 @@ export const NAVBAR = {
 
 interface DashboardHeaderProps {
     isCollapse: boolean;
-    isOffset?: boolean;
-    verticalLayout?: boolean;
     onOpenSidebar: VoidFunction
 }
 
 interface CustomAppBarProps extends AppBarProps {
     isCollapse?: boolean;
-    isOffset?: boolean;
-    verticalLayout?: boolean;
 }
 
 const RootStyle = styled(AppBar, {
-    shouldForwardProp: (prop) => prop !== 'isCollapse' && prop !== 'isOffset' && prop !== 'verticalLayout',
+    shouldForwardProp: (prop) => prop !== 'isCollapse',
 })<CustomAppBarProps>
 
-    (({ isCollapse, isOffset, verticalLayout, theme }) => ({
+    (({ isCollapse, theme }) => ({
         ...cssStyles().bgBlur(),
         boxShadow: 'none',
         height: HEADER.MOBILE_HEIGHT,
@@ -50,41 +46,34 @@ const RootStyle = styled(AppBar, {
             duration: theme.transitions.duration.shorter,
         }),
         [theme.breakpoints.up('lg')]: {
-            height: HEADER.DASHBOARD_DESKTOP_HEIGHT,
-            width: `calc(100% - ${NAVBAR.DASHBOARD_WIDTH + 1}px)`,
+            width: '100%',
+            height: HEADER.DASHBOARD_DESKTOP_OFFSET_HEIGHT,
+            backgroundColor: "#FFFFFF",
             ...(isCollapse && {
                 width: `calc(100% - ${NAVBAR.DASHBOARD_COLLAPSE_WIDTH}px)`,
-            }),
-            ...(isOffset && {
-                height: HEADER.DASHBOARD_DESKTOP_OFFSET_HEIGHT,
-            }),
-            ...(verticalLayout && {
-                width: '100%',
-                height: HEADER.DASHBOARD_DESKTOP_OFFSET_HEIGHT,
-                backgroundColor: theme.palette.background.default,
-            }),
+            })
         },
     }));
 
 
 
-export default function DashboardHeader({ onOpenSidebar, isCollapse, verticalLayout = false, isOffset }: DashboardHeaderProps) {
+export default function DashboardHeader({ onOpenSidebar, isCollapse }: DashboardHeaderProps) {
 
     const isDesktop = useResponsive('up', 'lg', 0, 0);
-    // const mdUp = useResponsive('up', 'md', 0, 0);
 
     return (
-        <RootStyle isCollapse={isCollapse} isOffset={isOffset} verticalLayout={true}>
+        <RootStyle isCollapse={isCollapse}>
             <Toolbar
                 sx={{
                     minHeight: '100% !important',
                     marginTop: 2,
                     px: { lg: 5 },
+                    backgroundColor: "#FFFFFF"
                 }}
             >
                 <Grid container spacing={0.5}>
                     <Grid item xs={12} display="flex" >
-                        {isDesktop && verticalLayout && <Logo sx={{ mr: 2.5 }} />}
+                        {isDesktop && <Logo sx={{ mr: 2.5 }} />}
                         {!isDesktop && (
                             <IconButtonAnimate onClick={onOpenSidebar} sx={{ mr: 1, color: 'text.primary' }}>
                                 <Iconify icon="eva:menu-2-fill" />
